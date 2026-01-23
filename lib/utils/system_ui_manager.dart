7e241653fe225c597e7f7b9a5b688ca154e8dc3d
haritsrs <haritssetiono23@gmail.com>
2026-01-23 17:21:10 +0700

    v1.2.7.1

--- lib/utils/system_ui_manager.dart
+++ lib/utils/system_ui_manager.dart
@@ -10,12 +10,16 @@ class SystemUIManager {
     systemNavigationBarColor: Colors.black,
     systemNavigationBarIconBrightness: Brightness.light,
     systemNavigationBarDividerColor: Colors.black,
-    systemNavigationBarContrastEnforced: false,
+    systemNavigationBarContrastEnforced: true,
   );
   
   static void initialize() {
     SystemChrome.setSystemUIOverlayStyle(_systemUIStyle);
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    // Edge-to-edge layout but with navigation bar visible (not transparent)
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.edgeToEdge,
+      overlays: [SystemUiOverlay.top, SystemUiOverlay.bottom],
+    );
   }
   
   static void enforce() {
